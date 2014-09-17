3cbd556dfc16f5990040c1b2a689a17df68d41d0
dlevy <djimmy.levy@gmail.com>
2014-09-17 14:19:09 +0200

    strrev

--- ft_strrev.c
+++ ft_strrev.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*   testrev.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlevy <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/09/16 23:49:14 by dlevy             #+#    #+#             */
-/*   Updated: 2014/09/17 02:03:22 by dlevy            ###   ########.fr       */
+/*   Created: 2014/09/17 13:58:20 by dlevy             #+#    #+#             */
+/*   Updated: 2014/09/17 14:15:55 by dlevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_strlen(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -26,24 +26,24 @@ char	*ft_strrev(char *str)
 {
 	int		i;
 	int		j;
-	char tmp; // tmp est juste un contenant, en (tres) gros, c'est a l'interieur que le swap va s'effectuer
+	char	tmp;
 
 	i = 0;
-	j = (ft_strlen(str) - 1);
+	j = ft_strlen(str) - 1;
 	while (i < j)
 	{
 		tmp = str[i];
 		str[i] = str[j];
 		str[j] = tmp;
-		j--;
 		i++;
+		j--;
 	}
+	return (str);
 }
 
 int		main(void)
 {
-	char	str[] = "Hello"; //str[] n'est pas a la norme, juste pour tester
-	ft_strrev(str);
-	printf("str = %s\n", str);
+	char str[] = "Hello";
+	printf("%s\n", ft_strrev(str));
 	return (0);
 }
